a6770d2197f79e32b82c73e068857f2b0717e114
da Vinci <brucewilson1103@gmail.com>
2019-03-06 13:42:37 -0500

    Updates

---
+++ precache-manifest.72aba0c0aed851557f2c25b7ce7a94d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.3fe81c4c.js"
+  },
+  {
+    "revision": "f3a0d7132d8cd1b50c3a",
+    "url": "/clickyGame/static/js/main.f3a0d713.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clickyGame/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "3e5f23e690aa03a01a16f45361679c57",
+    "url": "/clickyGame/index.html"
+  }
+];
